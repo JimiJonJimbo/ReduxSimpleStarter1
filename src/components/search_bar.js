40a04d376748560aab92b42f691156ce93f42c55
JimiJonJimbo <jimmyling3@gmail.com>
2018-03-07 18:15:35 -0800

    Finished lecture 19

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-// The above is equivalent to:
-// const Component = React.Component;
-// and means we don't have to use React. in the declaration below.
 
 class SearchBar extends Component {
-  render() {
-    // onChange is a prop.
-    // No parentheses needed around the argument because there's only one.
-    return <input onChange={event => console.log(event.target.value)} />;
+  constructor(props) {
+    super(props);
 
-    // The above is equivalent to doing
-    // return <input onChange={this.onInputChange} />;
-    // with the below onInputChange function.
+    this.state = { term: '' };
   }
 
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
