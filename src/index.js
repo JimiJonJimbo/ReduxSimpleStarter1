fa7e3c4b2fb3176e23c41e33de5898df24676196
JimiJonJimbo <jimmyling3@gmail.com>
2018-03-08 17:45:06 -0800

    Finished lecture 22

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDSKn_0clm1Q6VYDLrQ67XdN44kslc_ikQ';
 
-// Create a new component. This component should produce some HTML.
-// This can be thought of as a class, not an instance.
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// const App = function() { this is the old way, below is the ES6 way
-const App = () => {
-  return <SearchBar />;
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // The above is equivalent to
+      // this.setState({ videos: videos });
+      // It only works when the key and value have the same name.
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(<App />, document.querySelector('.container'));
