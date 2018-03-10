019c8a59ace6a481783ea90a48eff59c9ba84148
JimiJonJimbo <jimmyling3@gmail.com>
2018-03-09 16:54:18 -0800

    Finished lecture 25

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDSKn_0clm1Q6VYDLrQ67XdN44kslc_ikQ';
 
 class App extends Component {
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
